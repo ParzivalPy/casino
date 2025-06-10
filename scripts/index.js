8042eea16717c32629c2cc2a27fd4fc4c19a4eab
ParzivalPy <gabriel.arthus@icloud.com>
2025-06-10 20:07:39 +0200

    creating first attempt welcome page which offer only 1 game for now : BlackJack

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+// Change the index of .verso to pull it up during the flip animation
+
+const card = document.querySelector(".game-card");
+const verso = document.querySelector(".verso");
+card.addEventListener("transitionstart", () => {
+  if (verso.style.zIndex == "3") {
+    setTimeout(() => {
+      verso.style.zIndex = 1;
+    }, 300);
+  } else {
+    setTimeout(() => {
+      verso.style.zIndex = 3;
+    }, 300);
+  }
+});
